b233694797ee380e17d4691caac15c2aa1be9fff
Rob McBroom <github@skurfer.com>
2013-11-13 14:50:57 -0500

    Initial commit of the MailMate plug-in

---
+++ QSMailMate/QSMailMateMediator.h
@@ -0,0 +1,12 @@
+//
+//  QSMailMateMediator.h
+//  QSMailMate
+//
+//  Created by Rob McBroom on 2013/11/12.
+//
+
+#import "QSMailMediator.h"
+
+@interface QSMailMateMediator : NSObject <QSMailMediator>
+
+@end
